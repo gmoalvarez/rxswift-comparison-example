14bdccb4f2e8a6b1d05fea4663f384286ccc1b6c
Guillermo <galvarez22@me.com>
2017-06-21 14:55:59 -0700

    added unit testing

--- final/RxSwift Demo/RxSwiftViewModel.spec.swift
+++ final/RxSwift Demo/RxSwiftViewModel.spec.swift
@@ -1,21 +1,46 @@
-//import XCTest
-//import RxSwift
-//@testable import RxSwift_Demo
-//
-//class RxSwiftViewModel_spec: XCTestCase {
-//
-//  var viewModel: RxSwiftViewModel!
-//
-//  override func setUp() {
-//    super.setUp()
-//    viewModel = RxSwiftViewModel()
-//  }
-//
-//  override func tearDown() {
-//    super.tearDown()
-//  }
-//
-//  func testSkiButtonIsHiddenWhenOverFiveActivities() {
-//  }
-//
-//}
+import XCTest
+import RxSwift
+import RxBlocking
+@testable import RxSwift_Demo
+
+class RxSwiftViewModel_spec: XCTestCase {
+
+  var viewModel: RxSwiftViewModel!
+
+  override func setUp() {
+    super.setUp()
+    viewModel = RxSwiftViewModel()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+  }
+
+  func testSkiButtonIsHiddenWhenOverFiveActivities() {
+    let skiButtonIsHiddenBefore = try! viewModel.skiButtonIsHidden.asObservable().toBlocking().first()!
+    XCTAssert(skiButtonIsHiddenBefore == false)
+
+    viewModel.activityButtonPressed(activity: Emoji.surf)
+    viewModel.activityButtonPressed(activity: Emoji.car)
+    viewModel.activityButtonPressed(activity: Emoji.car)
+    viewModel.activityButtonPressed(activity: Emoji.beer)
+    viewModel.activityButtonPressed(activity: Emoji.beer)
+
+    let skiButtonIsHiddenAfter = try! viewModel.skiButtonIsHidden.asObservable().toBlocking().first()!
+    XCTAssert(skiButtonIsHiddenAfter == true)
+  }
+
+  func testSkiButtonIsHiddenWhenFourBeers() {
+    let skiButtonIsHiddenBefore = try! viewModel.skiButtonIsHidden.asObservable().toBlocking().first()!
+    XCTAssert(skiButtonIsHiddenBefore == false)
+
+    viewModel.activityButtonPressed(activity: Emoji.beer)
+    viewModel.activityButtonPressed(activity: Emoji.beer)
+    viewModel.activityButtonPressed(activity: Emoji.beer)
+    viewModel.activityButtonPressed(activity: Emoji.beer)
+
+    let skiButtonIsHiddenAfter = try! viewModel.skiButtonIsHidden.asObservable().toBlocking().first()!
+    XCTAssert(skiButtonIsHiddenAfter == true)
+  }
+
+}
