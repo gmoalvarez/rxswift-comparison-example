69b456b2649fe3d6644b6e0092d6c0d36f44a664
Guillermo <galvarez22@me.com>
2017-06-21 14:47:46 -0700

    updated RxSwift

---
+++ final/RxSwift Demo/RxSwiftViewModel.spec.swift
@@ -0,0 +1,21 @@
+//import XCTest
+//import RxSwift
+//@testable import RxSwift_Demo
+//
+//class RxSwiftViewModel_spec: XCTestCase {
+//
+//  var viewModel: RxSwiftViewModel!
+//
+//  override func setUp() {
+//    super.setUp()
+//    viewModel = RxSwiftViewModel()
+//  }
+//
+//  override func tearDown() {
+//    super.tearDown()
+//  }
+//
+//  func testSkiButtonIsHiddenWhenOverFiveActivities() {
+//  }
+//
+//}
